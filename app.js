96dd61ee88f4aa40fa2d500040736122abdc673f
Adiba Hasan <56604392+adibahasan11@users.noreply.github.com>
2021-12-08 11:35:57 +0600

    Some Changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-    res.send("Welcome Azure")
+    res.send("Welcome to Azure")
 })
 
 app.listen(8080, ()=> {
